7f3be128eabce4fca5a2784e39cd2eec0f9d3552
Kashapov Nail <Kashapov Nail>
2017-05-27 20:30:56 +0300

    Finished Login/Registration page

--- src/main/resources/static/js/sidenav.js
+++
@@ -1,6 +0,0 @@
-$('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 240
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    }
-  );
-  $('.collapsible').collapsible();
